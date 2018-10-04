02c59d32f02cffad4d07f5cbe559dff254069a43
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-10-04 11:54:20 +0200

    add about.json / change server port

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -9,6 +9,15 @@ function makeServer(port) {
 		response.sendFile(__dirname + '/public/html/index.html');
 	});
 
+	app.get('/about.json', function (request, response) {
+		const fs = require('fs');
+		const about = JSON.parse(fs.readFileSync('about.json', 'utf8'));
+
+		about.client.host = request.ip.split(':').pop();
+		about.server.current_time = Date.now() / 1000 | 0;
+		response.send(about);
+	});
+
 	return app.listen(port);
 };
 
