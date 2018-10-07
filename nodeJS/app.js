f8d60e2322a635dfeb7ff916a40431b1903a1520
Benoit Pingris <benoit.pingris@epitech.eu>
2018-10-07 23:41:19 +0200

    add bootstrap scss to use with our scss + start of login view

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,24 +1,15 @@
 // Server creation
 
+
 function makeServer(port) {
 	const express = require('express');
 	const app = express();
 	const server = require('http').Server(app);
+	const viewDir = `${__dirname}/public/html`;
 
 	app.use(express.static(__dirname + '/public'));
 
-	app.get('/', function (request, response) {
-		response.sendFile(__dirname + '/public/html/index.html');
-	});
-
-	app.get('/about.json', function (request, response) {
-		const fs = require('fs');
-		const about = JSON.parse(fs.readFileSync('about.json', 'utf8'));
-
-		about.client.host = request.ip.split(':').pop();
-		about.server.current_time = getUnixTime();
-		response.send(about);
-	});
+	require('./routes')(app);
 
 	return server.listen(port, () => {
 		console.log(`Server launched on ${server.address().address}${server.address().port}`);
@@ -26,17 +17,12 @@ function makeServer(port) {
 };
 
 
-// Timer functions
-
-function getUnixTime() {
-	return Date.now() / 1000 | 0;
-};
 
 
 // Start server
 
 const server = makeServer(3000);
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 io.on('connection', function(client) {
 	client.on('join', function() {
