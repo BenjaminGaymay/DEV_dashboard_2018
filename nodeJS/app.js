07948f440f5e88ec705f67c3455e82ca97a541fc
Benoit Pingris <benoit.pingris@epitech.eu>
2018-10-08 16:30:31 +0200

    add mongodb db

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,5 +1,15 @@
 // Server creation
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('COUCOU');
+});
+
 function makeServer(port) {
 	const express = require('express');
 	const session = require('express-session');
