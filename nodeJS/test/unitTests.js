149b73ea008db5dfb8bff875c9852d557f4cb83e
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-10-09 16:14:32 +0200

    update unitTests

--- nodeJS/test/unitTests.js
+++ nodeJS/test/unitTests.js
@@ -5,14 +5,19 @@ describe('Loading Express', function () {
   it('Responds to /', function testSlash(done) {
   request(app.server)
     .get('/')
-    .expect(200, done);
+    .expect(302, done);
+  });
+  it('Responds to /login', function testLogin(done) {
+    request(app.server)
+      .get('/login')
+      .expect(200, done);
   });
   it('Responds to 404', function testPath(done) {
     request(app.server)
       .get('/foo/bar')
       .expect(404, done);
   });
-  it('Responds to about.json', function testPath(done) {
+  it('Responds to about.json', function testAboutJSON(done) {
     request(app.server)
       .get('/about.json')
       .expect(200, done);
